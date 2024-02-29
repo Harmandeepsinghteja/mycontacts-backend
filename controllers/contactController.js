f2b88c7b80bf61740f963e0ad7e6c76477160a91
Harmandeep TEJA <harmandeep.teja@amadeus.com>
2024-02-29 22:22:15 +0530

    Error Handlers

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,41 +1,47 @@
 // @desc Get all Contacts
 // @route GET /api/contacts
 // @access Public
-const getContacts = (req, res) => { 
-    res.status(200).json({message:"Get all contacts"});
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
 };
-
 
 // @desc Create all Contacts
 // @route Post /api/contacts
 // @access Public
-const createContact = (req, res) => { 
-    res.status(201).json({message:"Create Contacts"});
+const createContact = (req, res) => {
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("Please fill in all fields");
+  }
+  res.status(201).json({ message: "Create Contacts" });
 };
 
 // @desc GET  Contact
 // @route GET /api/contacts/:id
 // @access Public
-const getContact = (req, res) => { 
-    res.status(200).json({message:`Get Contact for ${req.params.id}`});
-  }
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get Contact for ${req.params.id}` });
+};
 
 // @desc Update Contact
 // @route Put /api/contacts
 // @access Public
-const updateContact = (req, res) => { 
-    res.status(200).json({message:`Update Contacts for ${req.params.id}`});
-  }
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update Contacts for ${req.params.id}` });
+};
 
-
-  // @desc Create all Contacts
+// @desc Create all Contacts
 // @route Post /api/contacts
 // @access Public
-const deleteContact = (req, res) => { 
-    res.status(200).json({message:`Delete Contacts for ${req.params.id}`});
-}
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete Contacts for ${req.params.id}` });
+};
 
-
-
-module.exports = {getContacts,
-                createContact, getContact,  updateContact, deleteContact};
+module.exports = {
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const env = require('dotenv').config();
+const express = require("express");
+const errorHandler = require("./middleware/errorHandler.js");
+const env = require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
-app.use("/api/contacts", require("./routes/contactRoutes.js"));     
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
